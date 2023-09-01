90750c750d614228e3e16d50e258c339756120d4
Diana <diana221098@outlook.com>
2023-09-01 06:27:07 -0600

    Se modifico para agregar la excepcion

---
+++ Curso_Alura_2_bytebank_Heredado/src/SaldoInsuficienteException_15.java
@@ -0,0 +1,19 @@
+public class SaldoInsuficienteException_15 extends Exception{
+    /*La clase se crea para lanzar una exception y se extiende como hija de la clase padre
+    RuntimeException
+
+
+    +Pero al extenderse solo a excepcion nos obligara a tener que tratar ala bomba.
+     Recordemos que ahora sera checked-verificado*/
+
+
+    public SaldoInsuficienteException_15(String mensaje){
+        super(mensaje);
+    }
+    /* Aquí se pueden llamar a los constructores de la clase padre,
+   * para el caso de aquí se va a llamar al contructor que envie un mensaje
+   *
+   * -Se declara el contructor de "SaldoInsuficienteException" y cada vez que
+   * se lanze esta excepcion va a enviar un mensaje y él mediante el constructor
+   * de la clase padre que es RuntimeExceptin, él va a setear ese mensaje. */
+}
